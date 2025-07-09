78c3ca0b547a555fd3cd535b1df6fde4cba96f98
saurabhbalgude <114283063+saurabhbalgude@users.noreply.github.com>
2025-07-09 11:32:58 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-sudo docker build -t web .
-sudo docker run -it -d -p 81:80 web
-
-sudo docker build -t app .
-sudo docker run -it -d -p 4000:4000 app
+echo "Installing backend dependencies..."
+cd /home/ubuntu/backend
+npm install
